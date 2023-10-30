f835944ad0d751b7c3af6165a2892b852698b9b6
Cactuskiller <daniaayad728@gamil.com>
2023-10-30 15:23:10 +0300

    this is a test of vsCODEs github shortcuts

--- test2.java
+++ test2.java
@@ -1,5 +1,8 @@
 public class test2 {
   public static void main(String[] args) {
     System.out.println("This is another test");
+
+    //this is an update to VS codes shortcuts for git commits and pushes
+    
   }
 }
